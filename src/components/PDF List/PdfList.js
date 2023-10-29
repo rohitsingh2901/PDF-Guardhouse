46e8f70cd9695ab43004241d2122ecc60f9b168f
rohitsingh2901 <80673974+rohitsingh2901@users.noreply.github.com>
2023-10-29 23:41:12 +0530

    buy,purchased

--- src/components/PDF List/PdfList.js
+++ src/components/PDF List/PdfList.js
@@ -1,17 +1,45 @@
 import React from 'react';
+import './PDFList.css'
 
 const PDFList = () => {
-  const pdfs = ['sample1.pdf', 'sample2.pdf', 'sample3.pdf', 'sample4.pdf'];
+  const pdfs = [
+    { name: 'Sample PDF 1', file: 'PDFs/dummy (1).pdf', status: 1 },
+    { name: 'Sample PDF 2', file: 'PDFs/dummy (1).pdf', status: 1 },
+    { name: 'Sample PDF 3', file: 'PDFs/dummy (3).pdf', status: 0 },
+    { name: 'Sample PDF 4', file: 'PDFs/dummy (4).pdf', status: 0 },
+    { name: 'Sample PDF 5', file: 'PDFs/dummy (5).pdf', status: 0 },
+    { name: 'Sample PDF 6', file: 'PDFs/dummy (6).pdf', status: 0 },
+    { name: 'Sample PDF 7', file: 'PDFs/dummy (7).pdf', status: 0 },
+    { name: 'Sample PDF 8', file: 'PDFs/dummy (8).pdf', status: 0 },
+    { name: 'Sample PDF 9', file: 'PDFs/dummy (9).pdf', status: 0 },
+    { name: 'Sample PDF 10', file: 'PDFs/dummy (10).pdf', status: 0 }
+  ];
 
   return (
-    <div>
-      <h2>List of PDFs</h2>
-      <ul>
+    <div className="container flex justify-center items-center flex-col my-12">
+      <h2 className='font-bold my-4'>List of PDFs</h2>
+      <ul style={{ listStyleType: 'none', padding: 0 }}>
         {pdfs.map((pdf, index) => (
-          <li key={index}>
-            <a href={`PDFs/dummy (${index+1}).pdf`} target="_blank" rel="noopener noreferrer">
-              {pdf}
-            </a>
+          <li key={index} style={{ marginBottom: '10px' }}>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <div style={{width:"150px"}}>
+              {
+                pdf.status===0 ? (<span>{<button  className='btn navbtn btn-sm mx-2'>Buy Now</button>}</span>): (<>{<span  className='purchasedBtn mx-2'>Purchased <i class="fa fa-check" aria-hidden="true"></i></span>}</>)
+              }
+              </div>
+              {pdf.status===1 ? (<i title='unlocked' class="fa-solid fa-unlock text-green-600"></i>) : (<i title='locked' class="fa-solid fa-lock text-red-700"></i>)}
+              <img className='ml-2' src="https://cdn-icons-png.flaticon.com/512/9496/9496432.png?ga=GA1.1.877150888.1686539129" alt="PDF Icon" style={{ marginRight: '10px', width: '30px', height: '30px' }} />     
+              {
+                pdf.status===0 ? (
+                <a title='Please purchase first' href={false} target="_blank" rel="noopener noreferrer">
+                {pdf.name}
+                </a>
+                ):(<a href={pdf.file} target="_blank" rel="noopener noreferrer">
+                {pdf.name} 
+                </a>)
+              
+              }
+            </div>
           </li>
         ))}
       </ul>
@@ -20,3 +48,4 @@ const PDFList = () => {
 };
 
 export default PDFList;
+
--- src/components/User Dashboard/UserDashboard.js
+++ src/components/User Dashboard/UserDashboard.js
@@ -1,33 +1,65 @@
 import React from 'react';
 
 const UserDashboard = () => {
+  const pdfs = [
+    { name: 'Sample PDF 1', file: 'PDFs/dummy (1).pdf', status: 1 },
+    { name: 'Sample PDF 2', file: 'PDFs/dummy (1).pdf', status: 1 },
+    { name: 'Sample PDF 3', file: 'PDFs/dummy (3).pdf', status: 0 },
+    { name: 'Sample PDF 4', file: 'PDFs/dummy (4).pdf', status: 0 },
+    { name: 'Sample PDF 5', file: 'PDFs/dummy (5).pdf', status: 0 },
+    { name: 'Sample PDF 6', file: 'PDFs/dummy (6).pdf', status: 0 },
+    { name: 'Sample PDF 7', file: 'PDFs/dummy (7).pdf', status: 0 },
+    { name: 'Sample PDF 8', file: 'PDFs/dummy (8).pdf', status: 0 },
+    { name: 'Sample PDF 9', file: 'PDFs/dummy (9).pdf', status: 0 },
+    { name: 'Sample PDF 10', file: 'PDFs/dummy (10).pdf', status: 0 }
+  ];
   return (
-    <div>
-      <h2>User Dashboard</h2>
-      <div>
+    <>
+       <h2 className='text-center font-extrabold'>User Dashboard</h2>
+    <div className='row my-12'>
+      <div className='col-6 flex justify-center items-center flex-col'>
         <h3>Purchased PDFs</h3>
         <ul>
-          <li>Sample PDF 1</li>
-          <li>Sample PDF 2</li>
+          {pdfs.map((pdf, index) => (
+          <li key={index} style={{ marginBottom: '10px' }}>
+            {
+              pdf.status===1 ? (<div style={{ display: 'flex', alignItems: 'center' }}>
+              {pdf.status===1 ? (<i title='unlocked' class="fa-solid fa-unlock text-green-600"></i>) : (<i title='locked' class="fa-solid fa-lock text-red-700"></i>)}
+              <img className='ml-4' src="https://cdn-icons-png.flaticon.com/512/9496/9496432.png?ga=GA1.1.877150888.1686539129" alt="PDF Icon" style={{ marginRight: '10px', width: '30px', height: '30px' }} />     
+              {
+                pdf.status===0 ? (
+                <a title='Please purchase first' href={false} target="_blank" rel="noopener noreferrer">
+                {pdf.name} 
+                </a>
+                ):(<a href={pdf.file} target="_blank" rel="noopener noreferrer">
+                {pdf.name} 
+                </a>)
+              
+              }
+            </div>) : ('')
+            }
+          </li>
+        ))}
         </ul>
       </div>
-      <div>
+      <div className='col-6 flex justify-center items-center flex-col'>
         <h3>Account Settings</h3>
         <form>
           <label>
-            Change Password:
-            <input type="password" />
+            Update Email:
+            <input type="email" className="form-control" />
           </label>
           <br />
           <label>
-            Update Email:
-            <input type="email" />
+            Change Password:
+            <input type="password" className="form-control" />
           </label>
           <br />
-          <button type="submit">Save Changes</button>
+          <button  className='btn navbtn' type="submit">Save Changes</button>
         </form>
       </div>
     </div>
+    </>
   );
 };
 
